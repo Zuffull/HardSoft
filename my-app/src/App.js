0d1f00a0d70331db0ed66f45873dbcc356cd72d1
Zuffull <sophiasuroid@gmail.com>
2025-02-19 21:57:03 +0200

    1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,38 +1,11 @@
-import React from 'react';
-import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <Black />
-      <header className="App-header">
-      </header>
-      <Menu />
-    </div>
-  );
-}
-
-function Black() {
-  return (
-    <div className="black">
-      <div className="black-box"></div>
-      <svg className="small-black-box" viewBox="0 0 100 100">
-        <ellipse cx="50" cy="50" rx="37.505" ry="37.505" fill="#D26900" />
-        <path d="M50,50 L50,0 A50,50 0 0,1 100,50 Z" fill="black" />
-      </svg>
-    </div>
-  );
-}
-
-function Menu() {
-  return (
-    <div className="menu">
-      <a href="#tables">Таблиці</a>
-      <a href="#forms">Форми</a>
-      <a href="#queries">Запити</a>
-      <a href="#add">Додавання</a>
-      <a href="#delete">Видалення</a>
-      <a href="#edit">Редагування</a>
+    <div className="wrapper">
+      <Header />
+      <Footer />
     </div>
   );
 }
---
+++ my-app/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <div>
+            <span className='logo'>HardSoft</span>
+        </div>
+        <div className='presentation'>123</div>
+    </header>
+  )
+}
