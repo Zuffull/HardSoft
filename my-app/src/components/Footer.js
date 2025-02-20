fa8f241ec1d21f99f158f8a77f7f6219659b446f
Zuffull <sophiasuroid@gmail.com>
2025-02-20 13:57:59 +0200

    1

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer>
-        Всі права захищені &copy; 2025 
+        Всі права захиені &copy; 2025 
     </footer>
   )
 }
