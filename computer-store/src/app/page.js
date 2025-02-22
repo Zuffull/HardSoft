1da39a44fe61ec13dc14542fbbeb3939bf954e61
Zuffull <sophiasuroid@gmail.com>
2025-02-22 15:55:25 +0200

    1

--- computer-store/src/app/page.js
+++ computer-store/src/app/page.js
@@ -1,7 +1,18 @@
+import Bebra from './homepage/Bebra.json';
+
 export default function Home() {
+  
   return (
     <div>
-      Golovna storinka
+      <ul>
+        {Bebra.products.map((product) => {
+          return <li key={product.id}>
+          <div>Назва: {product.name}</div>
+          <div>Ціна: {product.price}</div>
+          <div>ID: {product.id}</div>
+        </li>
+        })}
+      </ul>
     </div>
-  );
+  )
 }
--- computer-store/src/components/Header.js
+++ computer-store/src/components/Header.js
@@ -5,6 +5,19 @@ export default function Header() {
     <header>
       <div className='header-container'>
         <div className='logo'>HardSoft</div>
+        <nav>
+          <ul>
+            <li>
+              <a href='#'>Головна</a>
+            </li>
+            <li>
+              <a href='#'>Про нас</a>
+            </li>
+            <li>
+              <a href='#'>Контакти</a>
+            </li>
+          </ul>
+          </nav>
       </div>
       <div className='presentation'></div>
     </header>
